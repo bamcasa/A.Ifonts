64c9be0aad8e974e3c24932000bc4df9007fe80f
bamcasa <combeesang@gmail.com>
2020-08-26 14:13:36 +0900

    크롭과 흑백변환을 합침

--- crop&convert_grayscale.py
+++ crop&convert_grayscale.py
@@ -3,7 +3,6 @@ import cv2
 src = cv2.imread("src.jpg", cv2.IMREAD_COLOR)
 
 W = 11
-H = 1
 
 inpormation = src.shape
 height = inpormation[0]
@@ -11,16 +10,19 @@ width = inpormation[1]
 print(height,width)
 a = int(width/W)
 a_1 = 0
-b = int(height/H)
-b_1 = 0
 
-for j in range(H):
-    for i in range(W):
-        roi = src[b_1:b, a_1:a]
-        cv2.imwrite(f"crop_image/{i}.jpg", roi)
-        a_1 = a
-        a += int(width / W)
-    b_1 = b
-    b += int(height / H)
-    a_1 = 0
-    a = int(width/W)
+for l in range(W):
+    roi = src[0:height, a_1:a]
+    dst = cv2.resize(roi, dsize=(120, 120), interpolation=cv2.INTER_AREA)
+    for i in range(120):
+        for j in range(120):
+            if dst.item(i, j, 0) + dst.item(i, j, 1) + dst.item(i, j, 2) > 500:
+                for m in range(3):
+                    dst.itemset(i, j, m, 255)
+            else:
+                for m in range(3):
+                    dst.itemset(i, j, m, 0)
+
+    cv2.imwrite(f"crop_image/{l}.jpg", dst)
+    a_1 = a
+    a += int(width / W)
