2edeb911ce9fb3ef04388f1af9d829c33be7e153
bamcasa <combeesang@gmail.com>
2020-08-28 01:21:14 +0900

    자음 모음 추출

--- division.py
+++ division.py
@@ -1,3 +1,6 @@
+import time
+start = time.time()
+
 import tensorflow.keras
 from PIL import Image, ImageOps
 import numpy as np
@@ -10,22 +13,23 @@ np.set_printoptions(suppress=True)
 model = np.arange(11, dtype=object)
 image = np.arange(11, dtype=object)
 text = "다람쥐헌쳇바퀴에타고파"
+text1 = "abcdefghijk"
 for i in range(11):
-    #model[i] = tensorflow.keras.models.load_model(f"model/{text[i]}.h5")  #모델파일 불러오기
+    model[i] = tensorflow.keras.models.load_model(f"model/{text[i]}.h5")  #모델파일 불러오기
     image[i] = Image.open(f"crop_image/{i}.jpg") # 이미지파일 불러오기
 
-
-for i in range(3):
-    model[i] = tensorflow.keras.models.load_model(f"model/{text[i]}.h5")  # 모델파일 불러오기
 
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 max_x_y = [0,0]
 
-for j in range(3):
+for j in range(11):
     max = [0, 0, 0]
     max_i = [0, 0, 0]
-    time = 1
+    #if j != 6:
+    #    time = 1
+    #else:
+    #    time = 1000
     for i in range(1000):
         size = random.randrange(80, 120)
         x = random.randrange(0, 224 - size)
@@ -45,11 +49,20 @@ for j in range(3):
         data[0] = normalized_image_array
 
         prediction = model[j].predict(data)
-        print(f"{i} : {prediction}")
+        """
+        if j == 3:
+            prediction[0][1] = prediction[0][1] + prediction[0][2]
+            prediction[0][2] = prediction[0][3]
+        """
+
+
+        #print(f"{j} {i} : {prediction}")
+        if i % 100 == 0:
+            print(f"{j} {i} : {prediction}")
         if prediction[0][0] > max[0]:
             max[0] = prediction[0][0]
             max_i[0] = i
-            if j == 2:
+            if j == 2 or j == 6:
                 max_x_y[0] = x + size
                 max_x_y[1] = y + size
         if prediction[0][1] > max[1]:
@@ -60,7 +73,7 @@ for j in range(3):
             max_i[2] = i
 
     for i in range(3):
-        if j == 0 and i == 2:
+        if (j == 0 or j == 5 or j == 7 or j == 8 or j == 9 or j == 10) and i == 2:
             break
         if j == 2 and i == 1:
             img = Image.open(f'crop_image/2.jpg')
@@ -68,14 +81,19 @@ for j in range(3):
             for k in range(0,max_x_y[0]):
                 for l in range(0,max_x_y[1]):
                     px[k, l] = (255, 255, 255)
-            img.save(f'correct_image/{text[j]}_{i}.png')
+            img.save(f'correct_image/{text1[j]}_{i}.jpg')
+            break
+        if j == 6 and i == 1:
+            img = Image.open(f'crop_image/6.jpg')
+            px = img.load()
+            for k in range(0,max_x_y[0]):
+                for l in range(0,max_x_y[1]):
+                    px[k, l] = (255, 255, 255)
+            img.save(f'correct_image/{text1[j]}_{i}.jpg')
             break
         correct_image = Image.open(f"glyphs/{max_i[i]}.jpg")
-        correct_image.save(f"correct_image/{text[j]}_{i}.jpg")
+        correct_image.save(f"correct_image/{text1[j]}_{i}.jpg")
         print(max_i[i], max[i])
         #correct_image.show()
 
-
-
-
-
+print("걸린시간 : ",time.time() - start)
